edcb21ec7c2cb3dd64d2f25c5756d199e81a8e6e
myrriame <myrriametiffany@gmail.com>
2021-02-20 16:09:21 -0500

    Movie Poster Search Project

---
+++ script.js
@@ -0,0 +1,39 @@
+var API_KEY = config.API_KEY
+
+
+
+var button = document.getElementById("submitButton");
+
+button.addEventListener("click", function (event) {
+    // prevents page from refreshing so we could see the console
+    event.preventDefault()
+    // search bar input from user
+    var movieName = document.getElementById("movieInput").value;
+    console.log(movieName);
+    // accounts for if the user typed a movie with more than one word then adds + sign between
+    var replaced = movieName.split(' ').join('+');
+    console.log(replaced)
+
+    var addPoster = (data) => {
+        // console.log(data) to get object
+
+        // **previous method**
+        // var poster = document.createElement("img")
+        // poster.src = data.Poster
+        // console.log(poster) to check if url is appearing
+        // document.body.appendChild(poster) didn't work: this adds to the previous movie poster
+        // **previous method**
+
+        var moviePoster = document.getElementById("movieImage")
+        moviePoster.src = data.Poster
+        moviePoster.style.display = "block";
+    }
+
+
+    fetch("http://www.omdbapi.com/?apikey=" + API_KEY + "&t=" + replaced)
+        // .then(reply => console.log(reply)) used to check status
+        .then(reply => reply.json())
+        // .then(data => console.log(data)) to get url info
+        .then(data => addPoster(data))
+        .catch(err => console.log("Poster Not Available"));
+});
